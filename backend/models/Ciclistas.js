0d23433d2b227e137e858e826fda5e9a9be47549
JoseK <dajoseva@gmail.com>
2023-07-18 09:29:36 -0500

    falta terminar

--- backend/models/Ciclistas.js
+++ backend/models/Ciclistas.js
@@ -1,20 +1,21 @@
-import mongoose from "mongoose";
+import {Schema,model,mongoose} from "mongoose";
 
 const ciclistasSchema = mongoose.Schema({
     nombre:{
         type:String,
-        required:true,
-        trim:true
+        required:[true, 'El nombre es requerido']
     },
     edad:{
         type:String,
-        required:true,
-        trim:true
+        required:[true, 'La edad es requerida']
     },
     estatura:{
         type:String,
-        required:true,
-        trim:true
+        required:[true, 'La estatura es requerida']
+    },
+    equipo:{
+        type:String,
+        required:[true, 'El equipo es requerido']
     }
     },
     {
--- backend/routes/ciclistas.routes.js
+++ backend/routes/ciclistas.routes.js
@@ -1,10 +1,18 @@
 import { getCiclistas , postCiclistas , deleteCiclistas , updateCiclistas } from "../controllers/ciclistas.controllers.js";
 import {Router} from "express";
+import {check} from "express-validator";
+import Equipo from "../models/Equipo.js";
+import validateDocuments from "../middlewares/validate.doocuments.js";
+
 
 const router = Router();
 
 router.get("/all", getCiclistas);
-router.post("/add", postCiclistas);
+router.post("/add",[
+    check('nombre','el nombre es obligatorio').not().isEmpty(),
+    check('edad','la edad es obligatorio').not().isEmpty(),
+    check('estatura','la estatura es obligatoria').not().isEmpty()
+], postCiclistas);
 router.delete("/del", deleteCiclistas);
 router.patch("/upd" , updateCiclistas);
 
